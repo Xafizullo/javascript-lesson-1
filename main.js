74176e31235c2fb2faf9629d4cd8bc20f22a21fb
6100303h <hafizulloabd2001@gmail.com>
2022-04-08 04:54:18 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+if (operator == "+") {
+  console.log(firstNumber + secondNumber);
+} else if (operator == "-") {
+  console.log(firstNumber - secondNumber);
+} else if (operator == "*") {
+  console.log(firstNumber * secondNumber);
+} else if (operator == "/") {
+  console.log(firstNumber / secondNumber);
+} else {
+  alert("Siz to'g'ri amal kiritmadingiz");
+}
